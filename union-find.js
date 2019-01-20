0eea74ffe54ad47cc9e0fc72dd7b205bb6b80e9d
Eduard Shavyrko <soc_35m@protonmail.com>
2019-01-20 18:06:52 +0200

    add class UnionFind - Weighted quick-union with path compression

--- union-find.js
+++ union-find.js
@@ -80,3 +80,36 @@ class WeightedQuickUnion {
         }
     }
 }
+// Weighted quick-union with path compression
+class UnionFind {
+    constructor(num) {
+        this.id = [];
+        this.sizes = [];
+        for (let i = 0; i < num; i++) {
+            this.id[i] = i;
+            this.sizes[i] = 0;
+        }
+    }
+    _root(el) {
+        while (el !== this.id[el]) {
+            this.id[el] = this.id[this.id[el]]; // <- only one extra line of code !
+            el = this.id(el);
+        }
+        return el;
+    }
+    connected(p, q) {
+        return this._root(p) === this._root(q);
+    }
+    union(p, q) {
+        const pRoot = this._root(p);
+        const qRoot = this._root(q);
+        if (pRoot === qRoot) return;
+        if  (this.sizes[pRoot] < this.sizes[qRoot]) {
+            this.id[pRoot] = qRoot;
+            this.sizes[qRoot] += this.sizes[pRoot];
+        }  else                {
+            this.id[qRoot] = pRoot;
+            this.sizes[pRoot] += this.sizes[qRoot];
+        }
+    }
+}
