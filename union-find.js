c70b25c646a4a1f4f8d28b440bee94c7f6486c95
Eduard Shavyrko <soc_35m@protonmail.com>
2019-01-20 16:58:03 +0200

    add class QuickUnion

--- union-find.js
+++ union-find.js
@@ -20,3 +20,26 @@ class QuickFindUF {
 }
 
 // Quick-find is too slow
+
+class QuickUnionUF {
+    constructor(num) {
+        this.id = [];
+        for (let i = 0; i < num; i++) {
+            this.id[i] = i;
+        }
+    }
+    _root(el) {
+        while (el !== this.id[el]) {
+            el = this.id(el);
+        }
+        return el;
+    }
+    connected(p, q) {
+        return this._root(p) === this._root(q);
+    }
+    union(p, q) {
+        const pRoot = this._root(p);
+        const qRoot = this._root(q);
+        this.id[pRoot] = qRoot;
+    }
+}
